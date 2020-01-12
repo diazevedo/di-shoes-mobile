b7b08a12e5ee96a0f6906443c70e609871bd1e00
Diego Azevedo <dapptn@hotmail.com>
2020-01-12 22:20:05 +1100

    Items amount

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import { SafeArea, Container, Logo, BasketContainer } from './styles';
+import {
+  SafeArea,
+  Container,
+  Logo,
+  BasketContainer,
+  ItemAmount,
+} from './styles';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 const Header = ({ navigation }) => {
@@ -12,7 +18,8 @@ const Header = ({ navigation }) => {
         </BasketContainer>
 
         <BasketContainer onPress={() => navigation.navigate('Cart')}>
-          <Icon name="shopping-basket" color="#FFF" size={36} />
+          <Icon name="shopping-basket" color="#FFF" size={24} />
+          <ItemAmount>1</ItemAmount>
         </BasketContainer>
       </Container>
     </SafeArea>
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components/native';
-import logo from '../../assets/images/Logo.png';
+import logo from '../../assets/images/logo-rocket.png';
 
 export const SafeArea = styled.SafeAreaView`
   flex: 0;
@@ -8,14 +8,35 @@ export const SafeArea = styled.SafeAreaView`
 `;
 
 export const Container = styled.View`
-  background-color: #000;
-  flex: 1;
   flex-direction: row;
+  flex: 1;
   padding: 20px;
   justify-content: space-between;
-  align-items: center;
 `;
 
-export const Logo = styled.Image.attrs({ source: logo, reziseMode: 'cover' })``;
+export const Logo = styled.Image.attrs({ source: logo, resizeMode: 'cover' })`
+  width: 185px;
+  height: 24px;
+`;
 
-export const BasketContainer = styled.TouchableOpacity``;
+export const BasketContainer = styled.TouchableOpacity`
+  height: 24px;
+  flex: 1;
+  align-items: flex-end;
+  justify-content: flex-end;
+`;
+
+export const ItemAmount = styled.Text`
+  color: #fff;
+  position: absolute;
+  text-align: center;
+  background-color: #6b52ae;
+  top: -8px;
+  right: -8px;
+  min-height: 18px;
+  min-width: 18px;
+  font-size: 12px;
+  padding: 2px;
+  border-radius: 9px;
+  overflow: hidden;
+`;
